32b510567cb622a634f402fea91c8d7e6a84cb84
somyote4 <somyote.4@gmail.com>
2019-07-06 22:11:58 +0700

    test 0.03

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os, sys
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-from flask_script import Manager, Server
+#from flask_script import Manager, Server
 from application import create_app
 app = create_app()
-manager = Manager(app)
+#manager = Manager(app)
 
 # Turn on debugger by default and reloader
-manager.add_command("runserver", Server(
-    use_debugger = True,
-    use_reloader = True,
-    host = os.getenv('IP', '0.0.0.0'),
-    port = int(os.getenv('PORT', 8080)))
-)
+#manager.add_command("runserver", Server(
+#    use_debugger = True,
+#    use_reloader = True,
+#    host = os.getenv('IP', '0.0.0.0'),
+#    port = int(os.getenv('PORT', 8080)))
+#)
 
-if __name__ == "__main__":
-    manager.run()
+#if __name__ == "__main__":
+#    manager.run()
